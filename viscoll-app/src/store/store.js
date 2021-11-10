20636e98f1529f13bf58d982027db097223f2d64
lilblizzard <lilblizzard97@gmail.com>
2021-11-10 16:57:11 -0500

    enable redux dev tracing

--- viscoll-app/src/store/store.js
+++ viscoll-app/src/store/store.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'development') {
   if (window.__REDUX_DEVTOOLS_EXTENSION__) {
     storeEnhancers = compose(
       storeEnhancers,
-      window.__REDUX_DEVTOOLS_EXTENSION__()
+      window.__REDUX_DEVTOOLS_EXTENSION__({trace: true})
     );
   }
 } else {
