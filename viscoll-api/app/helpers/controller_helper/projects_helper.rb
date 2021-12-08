1860f89705fa1079943e8c59dc0616de58a90e8e
lilblizzard <lilblizzard97@gmail.com>
2021-12-08 13:27:41 -0500

    turn unparseable JSON into empty manifest

--- viscoll-api/app/helpers/controller_helper/projects_helper.rb
+++ viscoll-api/app/helpers/controller_helper/projects_helper.rb
@@ -50,35 +50,39 @@ module ControllerHelper
 
     def getManifestInformation(url)
       images   = []
-      response = JSON.parse(Net::HTTP.get(URI(url)))
-      iiif_version = response["@context"]
-      if iiif_version.include? "2"
-        begin
-          response["sequences"][0]["canvases"].each do |canvas|
-            images.push({ label: canvas["label"],
-                          url: canvas["images"][0]["resource"]["service"]["@id"] })
+      begin
+        response = JSON.parse(Net::HTTP.get(URI(url)))
+        iiif_version = response["@context"]
+        if iiif_version.include? "2"
+          begin
+            response["sequences"][0]["canvases"].each do |canvas|
+              images.push({ label: canvas["label"],
+                            url: canvas["images"][0]["resource"]["service"]["@id"] })
+            end
+          rescue
+            return { name: "Unparseable manifest URL", images: images }
           end
-        rescue
-          return { name: "Unparseable manifest URL", images: images }
-        end
-        return { name: response["label"][0..150],
-                 images: images } unless response["label"].empty?
-        return { name: "Unnamed manifest", images: images }
-      elsif iiif_version.include? "3"
-        begin
-          response["items"].each do |item|
-            images.push({ label: item["label"].values.first[0],
-                          url: item["items"][0]["items"][0]["body"]["id"] })
+          return { name: response["label"][0..150],
+                   images: images } unless response["label"].empty?
+          return { name: "Unnamed manifest", images: images }
+        elsif iiif_version.include? "3"
+          begin
+            response["items"].each do |item|
+              images.push({ label: item["label"].values.first[0],
+                            url: item["items"][0]["items"][0]["body"]["id"] })
+            end
+          rescue
+            return { name: "Unparseable manifest URL", images: images }
           end
-        rescue
-          return { name: "Unparseable manifest URL", images: images }
+          unless response["label"].values.first[0].empty?
+            return { name: response["label"].values.first[0][0..150], images: images }
+          end
+          return { name: "Unnamed manifest", images: images }
+        else
+          raise "IIIF Version: #{iiif_version}"
         end
-        unless response["label"].values.first[0].empty?
-          return { name: response["label"].values.first[0][0..150], images: images }
-        end
-        return { name: "Unnamed manifest", images: images }
-      else
-        raise "IIIF Version: #{iiif_version}"
+      rescue
+        return { name: "Unparseable manifest URL", images: images }
       end
     end
 
