210e2256b9c90410cd1d9efd70c8329c85bc5491
lilblizzard <lilblizzard97@gmail.com>
2021-12-22 16:06:30 -0500

    remove rescue from create method and reformat

--- viscoll-api/app/controllers/groups_controller.rb
+++ viscoll-api/app/controllers/groups_controller.rb
@@ -4,82 +4,78 @@ class GroupsController < ApplicationController
   
   # POST /groups
   def create
+    noOfGroups       = additional_params.to_h[:noOfGroups]
+    memberOrder      = additional_params.to_h[:memberOrder]
+    parentGroupID    = additional_params.to_h[:parentGroupID]
+    noOfLeafs        = additional_params.to_h[:noOfLeafs]
+    conjoin          = additional_params.to_h[:conjoin]
+    oddMemberLeftOut = additional_params.to_h[:oddMemberLeftOut]
+    groupIDs         = additional_params.to_h[:groupIDs]
+    leafIDs          = additional_params.to_h[:leafIDs]
+    sideIDs          = additional_params.to_h[:sideIDs]
+    project_id       = group_params.to_h[:project_id]
+    order            = additional_params.to_h[:order]
+    # Validate group parameters
+    @additionalErrors   = validateAdditionalGroupParams(noOfGroups, parentGroupID, memberOrder, noOfLeafs, conjoin, oddMemberLeftOut)
+    hasAdditionalErrors = false
+    @additionalErrors.each_value do |value|
+      if value.length > 0
+        hasAdditionalErrors = true
+      end
+    end
+    if (hasAdditionalErrors)
+      render json: { additional: @additionalErrors }, status: :unprocessable_entity and return
+    end
+    @groupErrors = { project_id: [] }
+    if (project_id == nil)
+      @groupErrors[:project_id].push("not found")
+      render json: { group: @groupErrors }, status: :unprocessable_entity and return
+    end
     begin
-      noOfGroups = additional_params.to_h[:noOfGroups]
-      memberOrder = additional_params.to_h[:memberOrder]
-      parentGroupID = additional_params.to_h[:parentGroupID]
-      noOfLeafs = additional_params.to_h[:noOfLeafs]
-      conjoin = additional_params.to_h[:conjoin]
-      oddMemberLeftOut = additional_params.to_h[:oddMemberLeftOut]
-      groupIDs = additional_params.to_h[:groupIDs]
-      leafIDs = additional_params.to_h[:leafIDs]
-      sideIDs = additional_params.to_h[:sideIDs]
-      project_id = group_params.to_h[:project_id]
-      order = additional_params.to_h[:order]
-      # Validate group parameters
-      @additionalErrors = validateAdditionalGroupParams(noOfGroups, parentGroupID, memberOrder, noOfLeafs, conjoin, oddMemberLeftOut)
-      hasAdditionalErrors = false
-      @additionalErrors.each_value do |value|
-        if value.length>0
-          hasAdditionalErrors = true
-        end
-      end
-      if (hasAdditionalErrors)
-        render json: {additional: @additionalErrors}, status: :unprocessable_entity and return
-      end
-      @groupErrors = {project_id: []}
-      if (project_id == nil) 
-        @groupErrors[:project_id].push("not found")
-        render json: {group: @groupErrors}, status: :unprocessable_entity and return
-      end
-      begin
-        @project = Project.find(project_id)
-      rescue Exception => e
-        @groupErrors[:project_id].push("project not found with id "+project_id)
-        render json: {group: @groupErrors}, status: :unprocessable_entity and return
-      end
-      new_groups = []
-      new_group_ids = []
-      groupIDIndex = 0
-      parent_group = nil
-      if parentGroupID != nil
-        parent_group = @project.groups.find(parentGroupID)
-      end
-      # Create groups
-      noOfGroups.times do |i|
-        group = Group.new(group_params)
-        if groupIDs
-          group.id = groupIDs[i]
-        end
-        if parentGroupID != nil
-          group.parentID = parentGroupID
-          group.nestLevel = parent_group.nestLevel + 1
-        end
-        if group.save
-          new_groups.push(group)
-          new_group_ids.push(group.id.to_s)
-        else
-          render json: {group: group.errors}, status: :unprocessable_entity and return
-        end
-      end
-      # Add new group(s) to parent
-      if parentGroupID != nil
-        parent_group.add_members(new_group_ids, memberOrder)
-      end
-      # Add group(s) to global list 
-      @project.add_groupIDs(new_group_ids, order.to_i - 1)
-      # Add leaves inside each new group
-      new_groups.each_with_index do |group, index|
-        if noOfLeafs
-          if (leafIDs and sideIDs)
-            addLeavesInside(project_id, group, noOfLeafs, conjoin, oddMemberLeftOut, leafIDs[index*noOfLeafs..index*noOfLeafs+noOfLeafs-1], sideIDs[index*2*noOfLeafs..index*2*noOfLeafs+noOfLeafs*2-1])
-          else
-            addLeavesInside(project_id, group, noOfLeafs, conjoin, oddMemberLeftOut)
-          end
-        end
-      end
+      @project = Project.find(project_id)
     rescue Exception => e
-      render json: {error: e.message}, status: :unprocessable_entity and return
+      @groupErrors[:project_id].push("project not found with id " + project_id)
+      render json: { group: @groupErrors }, status: :unprocessable_entity and return
+    end
+    new_groups    = []
+    new_group_ids = []
+    groupIDIndex  = 0
+    parent_group  = nil
+    if parentGroupID != nil
+      parent_group = @project.groups.find(parentGroupID)
+    end
+    # Create groups
+    noOfGroups.times do |i|
+      group = Group.new(group_params)
+      if groupIDs
+        group.id = groupIDs[i]
+      end
+      if parentGroupID != nil
+        group.parentID  = parentGroupID
+        group.nestLevel = parent_group.nestLevel + 1
+      end
+      if group.save
+        new_groups.push(group)
+        new_group_ids.push(group.id.to_s)
+      else
+        render json: { group: group.errors }, status: :unprocessable_entity and return
+      end
+    end
+    # Add new group(s) to parent
+    if parentGroupID != nil
+      parent_group.add_members(new_group_ids, memberOrder)
+    end
+    # Add group(s) to global list
+    @project.add_groupIDs(new_group_ids, order.to_i - 1)
+    # Add leaves inside each new group
+    new_groups.each_with_index do |group, index|
+      if noOfLeafs
+        if (leafIDs and sideIDs)
+          addLeavesInside(project_id, group, noOfLeafs, conjoin, oddMemberLeftOut, leafIDs[index * noOfLeafs..index * noOfLeafs + noOfLeafs - 1], sideIDs[index * 2 * noOfLeafs..index * 2 * noOfLeafs + noOfLeafs * 2 - 1])
+        else
+          addLeavesInside(project_id, group, noOfLeafs, conjoin, oddMemberLeftOut)
+        end
+      end
     end
   end
 
