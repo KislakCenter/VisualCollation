f73db7fffc38a8e551216c18b75c5c62044c6cf4
lilblizzard <lilblizzard97@gmail.com>
2022-01-07 14:47:23 -0500

    update sides controller

--- viscoll-api/app/controllers/sides_controller.rb
+++ viscoll-api/app/controllers/sides_controller.rb
@@ -4,19 +4,15 @@ class SidesController < ApplicationController
   
   # PATCH/PUT /sides/1
   def update
-    begin
-      if !@side.update(side_params)
-        render json: @side.errors, status: :unprocessable_entity and return
-      end
-    rescue Exception => e
-      render json: {error: e.message}, status: :unprocessable_entity and return
+    if !@side.update(side_params)
+      render json: @side.errors, status: :unprocessable_entity and return
+      raise VCError, "Side (#{@side.id} could not be updated: #{@side.errors.join "\n"})"
     end
   end
 
 
   # PATCH/PUT /sides
   def updateMultiple
-    begin
       allSides = side_params_batch_update.to_h[:sides]
       # VALIDATIONS
       @errors = []
@@ -32,59 +28,45 @@ class SidesController < ApplicationController
         end
       end
       @project = Project.find(sides[0].project_id)
-      if (@project.user_id!=current_user.id)
-        render json: {error: ""}, status: :unauthorized and return
-      end
+      authorize_project! @project
       if haveErrors
-        render json: {sides: @errors}, status: :unprocessable_entity and return
+        raise VCError, "Errors occurred when updating: #{@errors.join "\n"}"
       end
       allSides.each_with_index do |side_params, index|
         side = sides[index]
         previousSideImage = side.image.clone
         if !side.update(side_params[:attributes])
-          render json: side.errors, status: :unprocessable_entity and return
+          raise VCError, "Errors occurred when updating sides: #{side.errors.join "\n"}"
         else
           # SPEICAL CASE FOR DIY IMAGE MAPPING
           if side_params[:attributes]["image"]
             newSideImage = side_params[:attributes]["image"].clone
             # If an image was linked, check if it was a DIY and link this Side to that Image
-            if newSideImage and !(newSideImage.empty?) and newSideImage["manifestID"]=="DIYImages"
-              imageID = newSideImage["url"].split("/")[-1].split("_", 2)[0]
-              begin
-                imageLinked = Image.find(imageID)
-                !(imageLinked.sideIDs.include?(side.id.to_s)) ? imageLinked.sideIDs.push(side.id.to_s) : nil
-                imageLinked.save
-              rescue Exception => e
-              end
+            if newSideImage and !(newSideImage.empty?) and newSideImage["manifestID"] == "DIYImages"
+              imageID     = newSideImage["url"].split("/")[-1].split("_", 2)[0]
+              imageLinked = Image.find(imageID)
+              !(imageLinked.sideIDs.include?(side.id.to_s)) ? imageLinked.sideIDs.push(side.id.to_s) : nil
+              imageLinked.save
             end
             # If an image was linked, check if this side was previously linked to a DIY Image and unlink this Side to that Image
             if newSideImage and !newSideImage.empty? and !previousSideImage.empty? and previousSideImage["manifestID"]=="DIYImages"
               imageID = previousSideImage["url"].split("/")[-1].split("_", 2)[0]
-              begin
                 imageUnlinked = Image.find(imageID)
                 if !imageLinked or imageLinked.id.to_s != imageUnlinked.id.to_s
                   imageUnlinked.sideIDs.include?(side.id.to_s) ? imageUnlinked.sideIDs.delete(side.id.to_s) : nil
                   imageUnlinked.save
                 end
-              rescue Exception => e
-              end
             end
             # If an Image was unlinked, check if it was a DIY and unlink this Side from the Image
             if newSideImage and newSideImage.empty? and !previousSideImage.empty? and previousSideImage["manifestID"]=="DIYImages"
               imageID = previousSideImage["url"].split("/")[-1].split("_", 2)[0]
-              begin
                 image = Image.find(imageID)
                 image.sideIDs.include?(side.id.to_s) ? image.sideIDs.delete(side.id.to_s) : nil
                 image.save
-              rescue Exception => e
-              end
             end
           end
         end
       end
-    rescue Exception => e
-      render json: {error: e.message}, status: :unprocessable_entity and return
-    end
   end
   
   # PUT /sides/generatePageNumber
@@ -109,15 +91,9 @@ class SidesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_side
-      begin
         @side = Side.find(params[:id])
         @project = Project.find(@side.project_id)
-        if (@project.user_id!=current_user.id)
-          render json: {error: ""}, status: :unauthorized and return
-        end
-      rescue Exception => e
-        render json: {error: "side not found"}, status: :not_found and return
-      end   
+        authorize_project! @project
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
