7d726ccd71e0a75ca2d3d4a145ff51c56a8388a7
lilblizzard <lilblizzard97@gmail.com>
2022-02-16 15:30:05 -0500

    edit button sizing

--- viscoll-app/src/components/infoBox/LeafInfoBox.js
+++ viscoll-app/src/components/infoBox/LeafInfoBox.js
@@ -662,8 +662,8 @@ export default class LeafInfoBox extends React.Component {
               primary
               fullWidth
               onClick={() => this.toggleOutsideBifoliaModal(true)}
-              label="Show outside facing bifolia"
-              style={{ marginBottom: 10 }}
+              label="Outside Bifolia"
+              style={{ marginBottom: 10}}
               tabIndex={this.props.tabIndex}
           />
       )
@@ -674,8 +674,8 @@ export default class LeafInfoBox extends React.Component {
               primary
               fullWidth
               onClick={() => this.toggleInsideBifoliaModal(true)}
-              label="Show inside facing bifolia"
-              style={{ marginBottom: 10 }}
+              label="Inside Bifolia"
+              style={{ marginBottom: 10}}
               tabIndex={this.props.tabIndex}
           />
       )
