f7121c6e757ed2002346a710fd28374c61d8e843
Mike Kanning <mkanning@upenn.edu>
2026-08-27 13:15:50 -0400

    update rspec-rails to v4. remove rspec-guard and shoulda-matchers gems.

--- viscoll-api/spec/rails_helper.rb
+++ viscoll-api/spec/rails_helper.rb
@@ -32,14 +32,6 @@ require 'rails_jwt_auth/spec_helpers'
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
-
 RSpec.configure do |config|
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
