73e434227d39545114ef52ec398fe9dcd7f0074f
Amrey Mathurin <mamrey@upenn.edu>
2026-08-28 14:06:09 -0400

    Use rails 7 defaults

--- viscoll-api/config/application.rb
+++ viscoll-api/config/application.rb
@@ -29,7 +29,7 @@ module ViscollApi
     # -- all .rb files in that directory are automatically loaded.
 
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 5.0
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
