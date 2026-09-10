fedbc9374599e7cbc117cd7c2bc94fdd6c590d46
Carla Galarza <cgalarza@upenn.edu>
2026-09-10 10:18:16 -0400

    Merge back in configuration settings that we removed by the app:update rake task

--- viscoll-api/config/puma.rb
+++ viscoll-api/config/puma.rb
@@ -32,3 +32,12 @@ plugin :tmp_restart
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+# Specifies the `worker_timeout` threshold that Puma will use to wait before
+# terminating a worker in development environments.
+#
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
