80efaf3fee29e438c65ee33416d6a2be0ec66b8b
Amrey Mathurin <mamrey@upenn.edu>
2026-09-21 10:00:17 -0400

    pin @babel/core version to resolve dependency mismatch after upgrading to react-scripts 4.0.3 - replace deprecated babel-polyfill with react-app-polyfill - remove babel dependencies already included in create react app

--- viscoll-app/src/index.js
+++ viscoll-app/src/index.js
@@ -1,3 +1,6 @@
+// This must be the first line in src/index.js
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
